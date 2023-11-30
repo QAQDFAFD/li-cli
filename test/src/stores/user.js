257bd32006dedc14760e6cf03987df888f9dd20e
QAQDFAFD <wy15195382735@163.com>
2023-11-30 14:18:50 +0800

    fix: 误传

--- test/src/stores/user.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-export const user = defineStore('userInfo', {
-    state: () => {
-        return {
-            username: '',
-            token: ''
-        }
-    }
-})
